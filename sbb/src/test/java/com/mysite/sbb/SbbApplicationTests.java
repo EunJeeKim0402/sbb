b957d6b22915e930c25c9d35532f88367d7fcde8
EUNJEE0402 <adserina@naver.com>
2024-09-09 08:41:02 +0900

    h2연결
    h2연결

--- sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,6 +1,10 @@
 package com.mysite.sbb;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,17 +18,36 @@ class SbbApplicationTests {
 
 	@Test
 	void testJpa() {
-		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가");
-		q1.setContent("sbb에대해 알고싶음");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);
-		
-		Question q2 = new Question();
-		q2.setSubject("스프링부트 모델 질문입니다");
-		q2.setContent("id는 자동생성되나요");
-		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);
+		Question q = this.questionRepository.findBySubjectAndContent("sbb가 무엇인가", "sbb에대해 알고싶음");
+		assertEquals(1, q.getId());
 	}
 
 }
+
+
+//Question q1 = new Question();
+//q1.setSubject("sbb가 무엇인가");
+//q1.setContent("sbb에대해 알고싶음");
+//q1.setCreateDate(LocalDateTime.now());
+//this.questionRepository.save(q1);
+//
+//Question q2 = new Question();
+//q2.setSubject("스프링부트 모델 질문입니다");
+//q2.setContent("id는 자동생성되나요");
+//q2.setCreateDate(LocalDateTime.now());
+//this.questionRepository.save(q2);
+
+
+//List<Question> all = this.questionRepository.findAll();
+//assertEquals(2, all.size());
+//
+//Question q = all.get(0);
+//assertEquals("sbb가 무엇인가요?", q.getSubject());
+
+
+
+//Optional<Question> oq = this.questionRepository.findById(1);
+//if(oq.isPresent()) {
+//	Question q = oq.get();
+//	assertEquals("sbb가 무엇인가", q.getSubject());
+//}
