7dc0079c8a324eb588600e6c925cd878520c7574
EUNJEE0402 <adserina@naver.com>
2024-10-08 08:39:23 +0900

    마크다운
    마크다운

--- sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -8,19 +8,47 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 
 import com.mysite.sbb.DataNotFoundException;
+import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.user.SiteUser;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 
 @RequiredArgsConstructor
 @Service
 public class QuestionService {
 	
 	private final QuestionRepository questionRepository;
+	
+	private Specification<Question> search(String kw) {
+		return new Specification<>() {
+			private static final long serialVersionUID = 1L;
+			
+			@Override
+			public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+				query.distinct(true); // 중복 제거
+				Join<Question, SiteUser> u1 = q.join("author", JoinType.LEFT);
+				Join<Question, Answer> a = q.join("answerList", JoinType.LEFT);
+				Join<Answer, SiteUser> u2 = q.join("author", JoinType.LEFT);
+				return cb.or(cb.like(q.get("subject"), "%" + kw + "%"), // 제목
+					   cb.like(q.get("content"), "%" + kw + "%"), // 내용
+					   cb.like(u1.get("username"), "%" + kw + "%"), // 질문 작성자
+					   cb.like(a.get("content"), "%" + kw + "%"), // 답변 내용
+					   cb.like(u2.get("username"), "%" + kw + "%")); // 답변 작성자
+			}
+		};
+	}
 	
 	public Page<Question> getList(int page) {
 		List<Sort.Order> sorts = new ArrayList<>();
@@ -56,6 +84,11 @@ public class QuestionService {
 	
 	public void delete(Question question) {
 		this.questionRepository.delete(question);
+	}
+	
+	public void vote(Question question, SiteUser siteUser) {
+		question.getVoter().add(siteUser);
+		this.questionRepository.save(question);
 	}
 
 }
