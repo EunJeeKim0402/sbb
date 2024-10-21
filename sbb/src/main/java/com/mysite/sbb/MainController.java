811cda1d2ccd6c37f61515b212a91263c21ba22e
EUNJEE0402 <adserina@naver.com>
2024-10-22 08:31:30 +0900

    mainDiv4
    mainDiv4

--- sbb/src/main/java/com/mysite/sbb/MainController.java
+++ sbb/src/main/java/com/mysite/sbb/MainController.java
@@ -1,13 +1,26 @@
 package com.mysite.sbb;
 
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionService;
+import com.mysite.sbb.user.UserService;
+
 @Controller
 public class MainController {
+	
+	private final QuestionService questionService;
+	private final UserService userService;
+	
+    public MainController(QuestionService questionService, UserService userService) {
+        this.questionService = questionService;
+        this.userService = userService;
+    }
 	
 	@GetMapping("/sbb")
 	@ResponseBody
@@ -21,7 +34,11 @@ public class MainController {
 	}
 	
 	@GetMapping("/main")
-	public String login() {
+	public String login(Model model, @RequestParam(value="page", defaultValue="0") int page,
+			@RequestParam(value = "kw", defaultValue = "") String kw) {
+		Page<Question> paging = this.questionService.getList(page, kw);
+		model.addAttribute("paging", paging);
+		model.addAttribute("kw", kw);
 	    return "main";
 	}
 }
