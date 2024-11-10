84ed4e5770c241581ddfc21be1e3b92e48a98595
EUNJEE0402 <adserina@naver.com>
2024-11-11 08:13:02 +0900

    product
    product

---
+++ sbb/src/main/java/com/mysite/sbb/product/Product.java
@@ -0,0 +1,43 @@
+package com.mysite.sbb.product;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.user.SiteUser;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer productId;
+	
+	@Column(columnDefinition = "TEXT")
+	private String productName;
+	
+	private String productType;
+	
+	private Integer productPrice;
+	
+	private String productImage;
+	
+	private LocalDateTime productCreateDt;
+	
+	private String productCreateId;
+	
+	private LocalDateTime productModifyDt;
+	
+}
